6de103feba160dac179a7ec7b71697320a5314c3
NITISH TOM MICHAEL <niteshmichael@gmail.com>
2019-06-10 10:22:59 +0530

    Create file_handling_across_dirs.py

---
+++ file_handling_across_dirs.py
@@ -0,0 +1,9 @@
+d=[]
+for image in os.listdir("/media/her/bh/images_to_json/images"):
+    if image not in e:
+        d.append(image)
+
+for image in d:
+    f="/media/her/bh/images_to_json/images/"+str(image)
+    g="/media/her/bh/images_to_json/images_undetected/"+str(image)
+    shutil.copyfile(f ,g)  
