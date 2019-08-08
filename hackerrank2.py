de874c8ce2bc24f489f66ba02e75c22c17f19c7a
NITISH TOM MICHAEL <niteshmichael@gmail.com>
2019-08-09 01:58:34 +0530

    Update hackerrank2.py

--- hackerrank2.py
+++ hackerrank2.py
@@ -1,6 +1,17 @@
+ #hacker rank problem2
 
 
+# Sample Input 0
 
+# 3
+# Krishna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
+
+# Sample Output 0
+
+# 56.00 (avg)
 
 
 if __name__ == '__main__':
